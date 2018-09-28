49a8678ae84ca2a8790a4db91710c3370aa16e28
nson95 <nicksonnenberg95@gmail.com>
2018-09-28 19:30:33 -0400

    Functioning user and vendor entity, repo, and controller.

--- src/main/java/com/prs/web/UserController.java
+++ src/main/java/com/prs/web/UserController.java
@@ -26,24 +26,24 @@ public class UserController {
 		Iterable<User> users = userRepository.findAll();
 		return users;
 	}
-	@GetMapping("/Get") //get method calling "users/get..." and then the parameters http://localhost:8080/Users/Get?id=2  saying you are getting the second user in your user table
-	public @ResponseBody Optional<User> getUser(@RequestParam int id) { //requesting parameters is indicated by "?"
-		Optional<User> user = userRepository.findById(id); //returning an Optional type
+	@GetMapping("/Get") 
+	public @ResponseBody Optional<User> getUser(@RequestParam int id) { 
+		Optional<User> user = userRepository.findById(id);
 		return user; 
 		
 	}
 	
-	@PostMapping("/Add") //pass in a JSON object to user and return the object that was created
+	@PostMapping("/Add") 
 	public @ResponseBody User addUser(@RequestBody User user) { 
 		return userRepository.save(user);
 	}
 	
-	@PostMapping("/Change") //updating a user
+	@PostMapping("/Change")
 	public @ResponseBody User updateUser(@RequestBody User user) { 
 		return userRepository.save(user);
 	}
 	
-	@PostMapping("/Remove") //delete a user
+	@PostMapping("/Remove")
 	public @ResponseBody String removeUser(@RequestBody User user) { 
 		userRepository.delete(user);
 		return "user deleted";
